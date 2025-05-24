63631d608a26e08adff52c904bfb884092af927c
guiller santos <guillersantos111@gmail.com>
2025-05-24 14:10:42 +0800

    ReInitialCommit

---
+++ TricycleFareAndPassengerManagement.Application/Features/Auth/Queries/GetCurrentUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TricycleFareAndPassengerManagement.Application.Common.DTOs;
+
+namespace TricycleFareAndPassengerManagement.Application.Features.Auth.Queries
+{
+    public class GetCurrentUserQuery : IRequest<UserDto?>
+    {
+        #region Properties
+
+        public Guid UserId { get; set; }
+
+        #endregion Properties
+    }
+}
